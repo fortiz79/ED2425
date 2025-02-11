d7721f5bc2cca244dbf87e5caac7225d4a628719
alumno <alumno@alumno-VirtualBox>
2025-02-11 18:53:25 +0100

    commit inicial

---
+++ ED2425/src/ed2425/ED2425.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed2425;
+
+/**
+ *
+ * @author alumno
+ */
+public class ED2425 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola ED");
+    }
+    
+}
